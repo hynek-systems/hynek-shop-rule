c5b2ab714b7fb78784f9c3c20f8b2585734a2bfe
Henric Söderlind <henric.soderlind@outlook.com>
2026-07-12 17:52:42 +0200

    Refactor RuleEvaluator

--- src/evaluators/rule-evaluator.ts
+++ src/evaluators/rule-evaluator.ts
@@ -1,37 +1,30 @@
 import { Group } from "../nodes/group.js";
 import { Rule } from "../nodes/rule.js";
 import { RuleTree } from "../tree/rule-tree.js";
+import { TraversingNodeVisitor } from "../visitors/traversing-node-visitor.js";
 
 import type { FieldResolver } from "./field-resolver.js";
 
-export class RuleEvaluator<T = unknown> {
-  public constructor(private readonly resolver: FieldResolver<T>) {}
+export class RuleEvaluator<T = unknown> extends TraversingNodeVisitor<boolean> {
+  public constructor(private readonly resolver: FieldResolver<T>) {
+    super();
+  }
+
+  #subject!: T;
 
   public evaluate(tree: RuleTree, subject: T): boolean {
-    return this.evaluateGroup(tree.root, subject);
+    this.#subject = subject;
+
+    return tree.root.accept(this);
   }
 
-  private evaluateGroup(group: Group, subject: T): boolean {
-    return group.operator.evaluate(this.evaluateChildren(group, subject));
+  protected override onGroup(group: Group, children: readonly boolean[]): boolean {
+    return group.operator.evaluate(children);
   }
 
-  private *evaluateChildren(group: Group, subject: T): Generator<boolean> {
-    for (const child of group.children) {
-      yield this.evaluateNode(child as any, subject);
-    }
-  }
+  protected override onRule(rule: Rule): boolean {
+    const left = this.resolver.resolve(this.#subject, rule.field);
 
-  private evaluateNode(node: Rule | Group, subject: T): boolean {
-    if (node instanceof Rule) {
-      return this.evaluateRule(node, subject);
-    }
-
-    return this.evaluateGroup(node, subject);
-  }
-
-  private evaluateRule(rule: Rule, subject: T): boolean {
-    const value = this.resolver.resolve(subject, rule.field);
-
-    return rule.operator.evaluate(value, rule.value);
+    return rule.operator.evaluate(left, rule.value);
   }
 }
