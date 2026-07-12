9dbb4c4c989b5063215adedc3f9279ca5531e1e3
Henric Söderlind <henric.soderlind@outlook.com>
2026-07-12 21:33:35 +0200

    Implement range in serialization and deserialization

--- src/serializer/rule-serializer.ts
+++ src/serializer/rule-serializer.ts
@@ -1,4 +1,5 @@
 import { Rule } from "../nodes/rule.ts";
+import { Range } from "../values/range.ts";
 
 import type { RuleDto } from "./types.ts";
 
@@ -8,7 +9,25 @@ export class RuleSerializer {
       type: "rule",
       field: rule.field,
       operator: rule.operator.id,
-      value: rule.value,
+      value: this.serializeOperand(rule.value),
+    };
+  }
+
+  private serializeOperand(value: unknown): unknown {
+    if (value instanceof Range) {
+      return this.serializeRange(value);
+    }
+
+    return value;
+  }
+
+  private serializeRange(range: Range<unknown>): {
+    from: unknown;
+    to: unknown;
+  } {
+    return {
+      from: range.from,
+      to: range.to,
     };
   }
 }
--- src/serializer/rule-tree-deserializer.ts
+++ src/serializer/rule-tree-deserializer.ts
@@ -1,9 +1,11 @@
 import { Group } from "../nodes/group.ts";
 import { Rule } from "../nodes/rule.ts";
 import { RuleTree } from "../tree/rule-tree.ts";
+import { Range } from "../values/range.ts";
 
 import type { GroupDto, RuleDto, RuleTreeDto } from "./types.ts";
 import type { RuleContext } from "../rule-context.ts";
+import { OperandKind } from "../operators/rule/operand-kind.ts";
 
 export class RuleTreeDeserializer {
   public constructor(private readonly context: RuleContext) {}
@@ -29,6 +31,27 @@ export class RuleTreeDeserializer {
   }
 
   private deserializeRule(dto: RuleDto): Rule {
-    return new Rule(dto.field, this.context.ruleOperators.get(dto.operator), dto.value);
+    const operator = this.context.ruleOperators.get(dto.operator);
+
+    return new Rule(dto.field, operator, this.deserializeOperand(operator.operandKind, dto.value));
+  }
+
+  private deserializeOperand(operandKind: OperandKind, value: unknown): unknown {
+    switch (operandKind) {
+      case OperandKind.Range:
+        return this.deserializeRange(value);
+
+      default:
+        return value;
+    }
+  }
+
+  private deserializeRange(value: unknown): Range<unknown> {
+    const range = value as {
+      from: unknown;
+      to: unknown;
+    };
+
+    return new Range(range.from, range.to);
   }
 }
