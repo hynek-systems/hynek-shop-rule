ffdfeeb57587419370241d976a16c0101cfe49d2
Henric Söderlind <henric.soderlind@outlook.com>
2026-07-13 12:56:13 +0200

    Refactor Field

--- src/fields/field.ts
+++ src/fields/field.ts
@@ -1,6 +1,15 @@
-export class Field<T = unknown> {
+export interface FieldOptions {
+  category?: string;
+
+  description?: string;
+
+  icon?: string;
+}
+
+export class Field {
   public constructor(
     public readonly id: string,
     public readonly label: string,
+    public readonly options: FieldOptions = {},
   ) {}
 }
--- tests/field.test.ts
+++ tests/field.test.ts
@@ -5,10 +5,24 @@ import { FieldExpression } from "../src/builders/field-expression.ts";
 
 describe("Field", () => {
   it("should return an instance of FieldExpression when calling Rule.field with a string", () => {
-    const field = new Field<string>("country", "Country");
+    const field = new Field("country", "Country");
 
     const expression = Rule.field(field);
 
     expect(expression).toBeInstanceOf(FieldExpression);
+  });
+
+  it("stores metadata", () => {
+    const field = new Field("price", "Price", {
+      category: "Product",
+      description: "Current product price",
+      icon: "currency",
+    });
+
+    expect(field.options.category).toBe("Product");
+
+    expect(field.options.description).toBe("Current product price");
+
+    expect(field.options.icon).toBe("currency");
   });
 });
